0712e7cc4573189c8297bf58542e3004a4201c8b
Suho Kwon <suho.kwon@gmail.com>
2023-01-26 18:01:21 +0900

    Change fibonacci.c.

--- Part01/fibonacci.c
+++ Part01/fibonacci.c
@@ -4,11 +4,11 @@ unsigned int fibonacci(unsigned int n) {
     unsigned int ret;
 
     if ((n == 0)||(n == 1)){
-        printf("Index : %d, result : %d\n", n, n);
+        printf("Index : %d, result : %d.\n", n, n);
         return n;
     }
     ret = fibonacci(n - 2) + fibonacci(n - 1);
-    printf("Index : %d, result : %d\n", n, ret);
+    printf("Index : %d, result : %d.\n", n, ret);
     return ret;
 }
 
