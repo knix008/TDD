4eaca6a92ead5f1b193bad66361018a106bb0ee8
Suho Kwon <suho.kwon@gmail.com>
2023-01-26 18:25:40 +0900

    Add CUnity example.

--- Part01/fibonacci/fibonacci.c
+++ Part01/fibonacci/fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "fibonacci.h"
 
 unsigned int fibonacci(unsigned int n) {
     unsigned int ret;
@@ -10,9 +11,4 @@ unsigned int fibonacci(unsigned int n) {
     ret = fibonacci(n - 2) + fibonacci(n - 1);
     printf("Index : %d, result : %d.\n", n, ret);
     return ret;
-}
-
-int main() {
-    unsigned int result = fibonacci(10);
-    printf("The fibonacci number : %d.\n", result);
 }
---
+++ Part01/fibonacci/fibonacci.h
@@ -0,0 +1,6 @@
+#ifndef _FIBONACCI_H_
+#define _FIBONACCI_H_
+
+unsigned int fibonacci(unsigned int n);
+
+#endif
