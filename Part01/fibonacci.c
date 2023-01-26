7c857d98b02be54b0ad16adbbef8f178519ffd4b
Suho Kwon <suho.kwon@gmail.com>
2023-01-26 17:58:45 +0900

    Change fibonacci.c.

---
+++ Part01/fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+unsigned int fibonacci(unsigned int n) {
+    unsigned int ret;
+
+    if ((n == 0)||(n == 1)){
+        printf("Index : %d, result : %d\n", n, n);
+        return n;
+    }
+    printf("Index : %d, result : %d\n", n, n);
+    ret = fibonacci(n - 2) + fibonacci(n - 1);
+    return ret;
+}
+
+int main() {
+    unsigned int result = fibonacci(10);
+    printf("The fibonacci number : %d.\n", result);
+}
