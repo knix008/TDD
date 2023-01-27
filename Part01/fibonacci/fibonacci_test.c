43042ab485a58e6851eb0156b7d360b120a2d38b
Suho Kwon <suho.kwon@gmail.com>
2023-01-27 10:22:07 +0900

    Add test case example.

--- Part01/fibonacci/fibonacci_test.c
+++ Part01/fibonacci/fibonacci_test.c
@@ -9,18 +9,20 @@ void tearDown(void) {
     // clean stuff up here
 }
 
-void test_function_should_doBlahAndBlah(void) {
+void test_function_fibonacci_10(void) {
     //test stuff
+    TEST_ASSERT(55 == fibonacci(10));
 }
 
-void test_function_should_doAlsoDoBlah(void) {
+void test_function_fibonacci_20(void) {
     //more test stuff
+    TEST_ASSERT(6765 == fibonacci(20));
 }
 
 // not needed when using generate_test_runner.rb
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_function_should_doBlahAndBlah);
-    RUN_TEST(test_function_should_doAlsoDoBlah);
+    RUN_TEST(test_function_fibonacci_10);
+    RUN_TEST(test_function_fibonacci_20);
     return UNITY_END();
 }
