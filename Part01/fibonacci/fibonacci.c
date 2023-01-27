0a87a7dceb82f9910b206cfdd2b91eebe7d1dd2c
Suho Kwon <suho.kwon@gmail.com>
2023-01-27 10:32:57 +0900

    Add some changes for brevity.

--- Part01/fibonacci/fibonacci.c
+++ Part01/fibonacci/fibonacci.c
@@ -5,10 +5,10 @@ unsigned int fibonacci(unsigned int n) {
     unsigned int ret;
 
     if ((n == 0)||(n == 1)){
-        printf("Index : %d, result : %d\n", n, n);
+        /*printf("Index : %d, result : %d\n", n, n);*/
         return n;
     }
     ret = fibonacci(n - 2) + fibonacci(n - 1);
-    printf("Index : %d, result : %d\n", n, ret);
+    /*printf("Index : %d, result : %d\n", n, ret);*/
     return ret;
 }
--- Part01/fibonacci/fibonacci_test.c
+++ Part01/fibonacci/fibonacci_test.c
@@ -3,19 +3,23 @@
 
 void setUp(void) {
     // set stuff up here
+    printf("You need to do something before running each test cases here.\n");
 }
 
 void tearDown(void) {
     // clean stuff up here
+    printf("You need to do something after running each test cases here.\n");
 }
 
 void test_function_fibonacci_10(void) {
     //test stuff
+    printf("This is the first test case.\n");
     TEST_ASSERT(55 == fibonacci(10));
 }
 
 void test_function_fibonacci_20(void) {
     //more test stuff
+    printf("This is the second test case.\n");
     TEST_ASSERT(6765 == fibonacci(20));
 }
 
