453e55360643623ca0402297810182798fcf6ec6
Suho Kwon <suho.kwon@gmail.com>
2023-01-29 17:38:09 +0900

    Add sprintf cpputest example makefile.

--- Part01/sprintf04_cpputest/sprintf_cpputest.cpp
+++ Part01/sprintf04_cpputest/sprintf_cpputest.cpp
@@ -1,9 +1,10 @@
-TEST(sprintf, NoFormatOperations)
-{
-    char output[5] = "";
+#include <CppUTest/CommandLineTestRunner.h>
+#include <stdint.h>
+#include <memory.h>
 
-    LONGS_EQUAL(3, sprintf(output, "hey"));
-    STRCMP_EQUAL("hey", output);
+int main(int ac, char** av){
+   int result = CommandLineTestRunner::RunAllTests(ac, av);
+   return result;
 }
 
 TEST_GROUP(sprintf)
@@ -15,3 +16,29 @@ TEST_GROUP(sprintf)
         memset(output, 0xaa, sizeof output);
         expected = "";
     }
+    void teardown()
+    {
+    }
+    void expect(const char *s)
+    {
+        expected = s;
+    }
+    void given(int charsWritten)
+    {
+        LONGS_EQUAL(strlen(expected), charsWritten);
+        STRCMP_EQUAL(expected, output);
+        BYTES_EQUAL(0xaa, output[strlen(expected) + 1]);
+    }
+};
+
+TEST(sprintf, NoFormatOperations)
+{
+    expect("hey");
+    given(sprintf(output, "hey"));
+}
+
+TEST(sprintf, InsertString)
+{
+    expect("Hello World\n");
+    given(sprintf(output, "%s\n", "Hello World"));
+}
