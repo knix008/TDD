6892bcece142f4bd0d1bada572704468e645868b
Suho Kwon <suho.kwon@gmail.com>
2023-02-01 10:28:04 +0900

    Add more examples.

---
+++ Part02/LedDriver_Refactor/Src/LedDriver.c
@@ -0,0 +1,66 @@
+#include <stdbool.h>
+#include "LedDriver.h"
+
+static uint16_t *ledsAddress;
+
+enum
+{
+    ALL_LEDS_ON = ~0,
+    ALL_LEDS_OFF = ~ALL_LEDS_ON
+};
+
+static uint16_t *ledsAddress;
+static uint16_t ledsImage;
+
+void LedDriver_Create(uint16_t *address)
+{
+    ledsAddress = address;
+    ledsImage = ALL_LEDS_OFF;
+    *ledsAddress = ledsImage;
+}
+
+void LedDriver_Destroy(void)
+{
+}
+
+static uint16_t convertLedNumberToBit(int ledNumber)
+{
+    return 1 << (ledNumber - 1);
+}
+
+static void updateHardware(void)
+{
+    *ledsAddress = ledsImage;
+}
+
+enum
+{
+    FIRST_LED = 1,
+    LAST_LED = 16
+};
+
+static bool IsLedOutOfBounds(int ledNumber)
+{
+    return (ledNumber < FIRST_LED) || (ledNumber > LAST_LED);
+}
+
+void LedDriver_TurnOn(int ledNumber)
+{
+    if (IsLedOutOfBounds(ledNumber))
+        return;
+    ledsImage |= convertLedNumberToBit(ledNumber);
+    updateHardware();
+}
+void LedDriver_TurnOff(int ledNumber)
+{
+    if (IsLedOutOfBounds(ledNumber))
+        return;
+    ledsImage &= ~(convertLedNumberToBit(ledNumber));
+    updateHardware();
+}
+
+void LedDriver_TurnAllOn(void)
+{
+    ledsImage = ALL_LEDS_ON;
+    updateHardware();
+}
