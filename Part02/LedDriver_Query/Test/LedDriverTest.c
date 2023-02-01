5444075278d058d6dc5af5fa0b4c8cba29637085
Suho Kwon <suho.kwon@gmail.com>
2023-02-01 14:28:32 +0900

    Add more examples for LED Driver TDD.

---
+++ Part02/LedDriver_Query/Test/LedDriverTest.c
@@ -0,0 +1,158 @@
+#include "unity_fixture.h"
+#include "RuntimeError.h"
+#include "RuntimeErrorStub.h"
+#include "LedDriver.h"
+
+TEST_GROUP(LedDriver);
+
+static uint16_t virtualLeds;
+
+TEST_SETUP(LedDriver)
+{
+    LedDriver_Create(&virtualLeds);
+}
+
+TEST_TEAR_DOWN(LedDriver)
+{
+}
+
+TEST(LedDriver, LedsOffAfterCreate)
+{
+    uint16_t virtualLeds = 0xffff;
+
+    LedDriver_Create(&virtualLeds);
+    TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+}
+
+TEST(LedDriver, TurnOnLedOne)
+{
+    LedDriver_TurnOn(1);
+    TEST_ASSERT_EQUAL_HEX16(1, virtualLeds);
+}
+
+TEST(LedDriver, TurnOffLedOne)
+{
+    LedDriver_TurnOn(1);
+    LedDriver_TurnOff(1);
+    TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+}
+
+TEST(LedDriver, TurnOnMultipleLeds)
+{
+    LedDriver_TurnOn(9);
+    LedDriver_TurnOn(8);
+    TEST_ASSERT_EQUAL_HEX16(0x180, virtualLeds);
+}
+
+TEST(LedDriver, TurnOffAnyLed)
+{
+    LedDriver_TurnAllOn();
+    LedDriver_TurnOff(8);
+    TEST_ASSERT_EQUAL_HEX16(0xff7f, virtualLeds);
+}
+
+TEST(LedDriver, AllOn)
+{
+    LedDriver_TurnAllOn();
+    TEST_ASSERT_EQUAL_HEX16(0xffff, virtualLeds);
+}
+
+TEST(LedDriver, LedMemoryIsNotReadable)
+{
+    virtualLeds = 0xffff;
+
+    LedDriver_TurnOn(8);
+    TEST_ASSERT_EQUAL_HEX16(0x80, virtualLeds);
+}
+
+TEST(LedDriver, UpperAndLowerBounds)
+{
+    LedDriver_TurnOn(1);
+    LedDriver_TurnOn(16);
+    TEST_ASSERT_EQUAL_HEX16(0x8001, virtualLeds);
+}
+
+TEST(LedDriver, OutOfBoundsChangesNothing)
+{
+    LedDriver_TurnOn(-1);
+    LedDriver_TurnOn(0);
+    LedDriver_TurnOn(17);
+    LedDriver_TurnOn(3141);
+    TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+}
+
+/*
+TEST(LedDriver, OutOfBoundsChangesNothingWithCheck)
+{
+    LedDriver_TurnOn(-1);
+    TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+    LedDriver_TurnOn(0);
+    TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+    LedDriver_TurnOn(17);
+    TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+    LedDriver_TurnOn(33);
+    TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+    LedDriver_TurnOn(3141);
+    TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+}
+*/
+
+TEST(LedDriver, OutOfBoundsTurnOffDoesNoHarm)
+{
+    LedDriver_TurnAllOn();
+
+    LedDriver_TurnOff(-1);
+    LedDriver_TurnOff(0);
+    LedDriver_TurnOff(17);
+    LedDriver_TurnOff(3141);
+    TEST_ASSERT_EQUAL_HEX16(0xffff, virtualLeds);
+}
+
+TEST(LedDriver, OutOfBoundsProducesRuntimeError)
+{
+    LedDriver_TurnOn(-1);
+    RUNTIME_ERROR("LED Driver: out-of-bounds LED", -1);
+    TEST_ASSERT_EQUAL_STRING("LED Driver: out-of-bounds LED",
+                             RuntimeErrorStub_GetLastError());
+    TEST_ASSERT_EQUAL(-1, RuntimeErrorStub_GetLastParameter());
+}
+
+IGNORE_TEST(LedDriver, OutOfBoundsToDo)
+{
+    /* TODO: what should we do during runtime? */
+}
+
+TEST(LedDriver, IsOn)
+{
+    TEST_ASSERT_FALSE(LedDriver_IsOn(11));
+    LedDriver_TurnOn(11);
+    TEST_ASSERT_TRUE(LedDriver_IsOn(11));
+}
+
+TEST_GROUP_RUNNER(LedDriver)
+{
+    RUN_TEST_CASE(LedDriver, LedsOffAfterCreate);
+    RUN_TEST_CASE(LedDriver, TurnOnLedOne);
+    RUN_TEST_CASE(LedDriver, TurnOffLedOne);
+    RUN_TEST_CASE(LedDriver, TurnOnMultipleLeds);
+    RUN_TEST_CASE(LedDriver, TurnOffAnyLed);
+    RUN_TEST_CASE(LedDriver, AllOn);
+    RUN_TEST_CASE(LedDriver, LedMemoryIsNotReadable);
+    RUN_TEST_CASE(LedDriver, UpperAndLowerBounds);
+    RUN_TEST_CASE(LedDriver, OutOfBoundsChangesNothing);
+    /*RUN_TEST_CASE(LedDriver, OutOfBoundsChangesNothingWithCheck);*/
+    RUN_TEST_CASE(LedDriver, OutOfBoundsTurnOffDoesNoHarm);
+    RUN_TEST_CASE(LedDriver, OutOfBoundsProducesRuntimeError);
+    RUN_TEST_CASE(LedDriver, OutOfBoundsToDo);
+    RUN_TEST_CASE(LedDriver, IsOn);
+}
+
+static void RunAllTests(void)
+{
+    RUN_TEST_GROUP(LedDriver);
+}
+
+int main(int argc, const char *argv[])
+{
+    return UnityMain(argc, argv, RunAllTests);
+}
---
+++ Part02/LedDriver_QueryGuard_Done/Src/LedDriver.h
@@ -0,0 +1,14 @@
+#ifndef _LEDDRIVER_H_
+#define _LEDDRIVER_H_
+#include <stdint.h>
+#include <stdbool.h>
+
+void LedDriver_Create(uint16_t *address);
+void LedDriver_Destroy(void);
+void LedDriver_TurnOn(int ledNumber);
+void LedDriver_TurnOff(int ledNumber);
+void LedDriver_TurnAllOn(void);
+bool LedDriver_IsOn(int ledNumber);
+bool LedDriver_IsOff(int ledNumber);
+
+#endif
