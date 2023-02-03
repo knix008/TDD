92efd0c5c68f12b1182f2870cc3775dd2e3a181f
Suho Kwon <suho.kwon@gmail.com>
2023-02-03 18:33:52 +0900

    Add the first LightScheduler example.

---
+++ Part03/LightScheduler/SettingUp/LightScheduler.cpp
@@ -0,0 +1,11 @@
+#include "LightScheduler.hpp"
+
+void LightScheduler_ScheduleTurnOn(int, int, int)
+{
+    return;
+}
+
+void LightScheduler_Wakeup(void)
+{
+    return;
+}
---
+++ Part03/LightScheduler/SettingUp/LightSchedulerTest.cpp
@@ -0,0 +1,29 @@
+#include <CppUTest/CommandLineTestRunner.h>
+#include "LightScheduler.hpp"
+
+TEST_GROUP(LightScheduler){
+    void setup(){} void teardown(){}};
+
+/* 
+TEST(LightScheduler, ScheduleOnEverydayNotTimeYet)
+{
+   LightScheduler_ScheduleTurnOn(3, EVERYDAY, 1200);
+   FakeTimeService_SetDay(MONDAY);
+   FakeTimeService_SetMinute(1199);
+   LightScheduler_Wakeup();
+   LONGS_EQUAL(LIGHT_ID_UNKNOWN, LightControllerSpy_GetLastId());
+   LONGS_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_GetLastState());
+}
+*/
+
+TEST(LightScheduler, NoChangeToLightsDuringInitialization)
+{
+   LONGS_EQUAL(LIGHT_ID_UNKNOWN, LightControllerSpy_GetLastId());
+   LONGS_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_GetLastState());
+}
+
+int main(int ac, char **av)
+{
+   int result = CommandLineTestRunner::RunAllTests(ac, av);
+   return result;
+}
