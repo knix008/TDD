c731e50d539e0090d29f9582347811336c9dfae0
Suho Kwon <suho.kwon@gmail.com>
2023-02-07 14:13:49 +0900

    Add multiple events example.

---
+++ Part03/LightScheduler/Multiple/LightScheduler.c
@@ -0,0 +1,123 @@
+#include <stdbool.h>
+#include "TimeService.h"
+#include "LightController.h"
+#include "LightScheduler.h"
+
+enum
+{
+    UNUSED = -1,
+    TURN_OFF,
+    TURN_ON,
+    MAX_EVENTS = 128
+};
+
+typedef struct
+{
+    int id;
+    int event;
+    Day day;
+    int minuteOfDay;
+} ScheduledLightEvent;
+
+static ScheduledLightEvent scheduledEvent;
+static ScheduledLightEvent scheduledEvents[MAX_EVENTS];
+
+static void scheduleEvent(int id, Day day, int minuteOfDay, int event)
+{
+    int i;
+
+    for (i = 0; i < MAX_EVENTS; i++)
+    {
+        if (scheduledEvents[i].id == UNUSED)
+        {
+            scheduledEvents[i].day = day;
+            scheduledEvents[i].minuteOfDay = minuteOfDay;
+            scheduledEvents[i].event = event;
+            scheduledEvents[i].id = id;
+            break;
+        }
+    }
+
+    scheduledEvent.day = day;
+    scheduledEvent.minuteOfDay = minuteOfDay;
+    scheduledEvent.event = event;
+    scheduledEvent.id = id;
+}
+
+static void operateLight(ScheduledLightEvent *lightEvent)
+{
+    if (lightEvent->event == TURN_ON)
+        LightController_On(lightEvent->id);
+    else if (lightEvent->event == TURN_OFF)
+        LightController_Off(lightEvent->id);
+}
+
+static bool DoesLightRespondToday(Time *time, int reactionDay)
+{
+    int today = time->dayOfWeek;
+
+    if (reactionDay == EVERYDAY)
+        return true;
+    if (reactionDay == today)
+        return true;
+    if (reactionDay == WEEKEND && (SATURDAY == today || SUNDAY == today))
+        return true;
+    if (reactionDay == WEEKDAY && today >= MONDAY && today <= FRIDAY)
+        return true;
+    return false;
+}
+
+static void processEventDueNow(Time *time, ScheduledLightEvent *lightEvent)
+{
+    if (lightEvent->id == UNUSED)
+        return;
+    if (!DoesLightRespondToday(time, lightEvent->day))
+        return;
+    if (lightEvent->minuteOfDay != time->minuteOfDay)
+        return;
+
+    operateLight(lightEvent);
+}
+
+void LightScheduler_Create(void)
+{
+    int i;
+    scheduledEvent.id = UNUSED;
+
+    for (i = 0; i < MAX_EVENTS; i++)
+        scheduledEvents[i].id = UNUSED;
+
+    TimeService_SetPeriodicAlarmInSeconds(60, LightScheduler_Wakeup);
+}
+
+void LightScheduler_ScheduleTurnOn(int id, Day day, int minuteOfDay)
+{
+    scheduleEvent(id, day, minuteOfDay, TURN_ON);
+}
+
+void LightScheduler_ScheduleTurnOff(int id, Day day, int minuteOfDay)
+{
+    scheduleEvent(id, day, minuteOfDay, TURN_OFF);
+}
+
+void LightScheduler_Wakeup(void)
+{
+    int i;
+    Time time;
+
+    TimeService_GetTime(&time);
+    for (i = 0; i < MAX_EVENTS; i++)
+    {
+        processEventDueNow(&time, &scheduledEvents[i]);
+    }
+    processEventDueNow(&time, &scheduledEvent);
+}
+
+void LightScheduler_Destroy(void)
+{
+    TimeService_CancelPeriodicAlarmInSeconds(60, LightScheduler_Wakeup);
+}
+
+void LightScheduler_ScheduleRemove(int id, Day day, int minuteOfDay)
+{
+}
