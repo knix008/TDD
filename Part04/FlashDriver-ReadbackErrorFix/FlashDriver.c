a301c4d9ac5631a159087dcda8de2b194767df31
Suho Kwon <suho.kwon@gmail.com>
2023-02-13 11:24:01 +0900

    Add MicroTime example.

---
+++ Part04/FlashDriver-ReadbackErrorFix/FlashDriver.c
@@ -0,0 +1,37 @@
+#include "IO.h"
+#include "FlashDriver.h"
+
+void Flash_Create(void)
+{
+}
+
+void Flash_Destroy(void)
+{
+}
+
+static int writeError(int status)
+{
+    IO_Write(CommandRegister, Reset);
+    if (status & VppErrorBit)
+        return FLASH_VPP_ERROR;
+    else if (status & ProgramErrorBit)
+        return FLASH_PROGRAM_ERROR;
+    else if (status & BlockProtectionErrorBit)
+        return FLASH_PROTECTED_BLOCK_ERROR;
+    else
+        return FLASH_UNKNOWN_PROGRAM_ERROR;
+}
+
+int Flash_Write(ioAddress offset, ioData data)
+{
+    ioData status = 0;
+    IO_Write(CommandRegister, ProgramCommand);
+    IO_Write(offset, data);
+    while ((status & ReadyBit) == 0)
+        status = IO_Read(StatusRegister);
+    if (status != ReadyBit)
+        return writeError(status);
+    if (data != IO_Read(offset))
+        return FLASH_READ_BACK_ERROR;
+    return FLASH_SUCCESS;
+}
---
+++ Part04/MicroTime/MicroTime.h
@@ -0,0 +1,8 @@
+#ifndef __MICROTIME_H__
+#define __MICROTIME_H__
+#include <stdint.h>
+
+uint32_t MicroTime_Get(void);
+void MicroTime_Delay(uint32_t);
+
+#endif
