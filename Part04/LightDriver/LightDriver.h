a7af152040b7d1d6be97ba2da7a8ef7147b0bf9f
Suho Kwon <suho.kwon@gmail.com>
2023-02-13 19:13:18 +0900

    Add device directory.

--- Part04/LightDriver/LightDriver.h
+++ Part04/LightDriver/LightDriver.h
@@ -3,14 +3,6 @@
 
 typedef struct LightDriverStruct *LightDriver;
 
-typedef enum LightDriverType
-{
-    TestLightDriver,
-    X10,
-    AcmeWireless,
-    MemoryMapped
-} LightDriverType;
-
 void LightDriver_Destroy(LightDriver);
 void LightDriver_TurnOn(LightDriver);
 void LightDriver_TurnOff(LightDriver);
