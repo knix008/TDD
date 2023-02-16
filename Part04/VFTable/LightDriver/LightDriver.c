ba41442950abb4168fb1a197c04cc9b1777d1251
Suho Kwon <suho.kwon@gmail.com>
2023-02-16 10:35:19 +0900

    Add code changes.

--- Part04/VFTable/LightDriver/LightDriver.c
+++ Part04/VFTable/LightDriver/LightDriver.c
@@ -9,54 +9,43 @@ void LightDriver_SetInterface(LightDriverInterface i)
     interface = i;
 }
 
-/*
-static bool isValid(LightDriver self)
-{
-    return interface && self;
-}
-
 void LightDriver_TurnOn(LightDriver self)
 {
-    if (isValid(self))
-    {
-        interface->TurnOn(self);
-    }
-}
-*/
-
-void LightDriver_TurnOn(LightDriver self)
-{
+    /*
     if (self)
     {
         if(self->vtable->TurnOn)
             self->vtable->TurnOn(self);
     }
+    */
+    if (self && self->vtable && self->vtable->TurnOn)
+        self->vtable->TurnOn(self);
 }
 
 void LightDriver_TurnOff(LightDriver self)
 {
+    /*
     if (self)
     {
         if(self->vtable->TurnOff)
             self->vtable->TurnOn(self);
     }
+    */
+    if (self && self->vtable && self->vtable->TurnOff)
+        self->vtable->TurnOff(self);
 }
-
-/*
-void LightDriver_SetBrightness(LightDriver self, int level)
-{
-    if (isValid(self) && self->brightness)
-        self->brightness(self, level);
-}
-*/
 
 void LightDriver_Destroy(LightDriver self)
 {
+    /*
     if (self)
     {
         if(self->vtable->Destroy)
             self->vtable->Destroy(self);
     }
+    */
+   if (self && self->vtable && self->vtable->Destroy)
+        self->vtable->Destroy(self);
 }
 
 const char *LightDriver_GetType(LightDriver driver)
