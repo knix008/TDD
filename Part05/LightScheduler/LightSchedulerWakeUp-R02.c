2585e4a8e37fd519d2e5a169d5cdd3c37a4c6dff
Suho Kwon <suho.kwon@gmail.com>
2023-02-20 10:17:31 +0900

    Add WakeUp Function refacting examples.

--- Part05/LightScheduler/LightSchedulerWakeUp-R02.c
+++ Part05/LightScheduler/LightSchedulerWakeUp-R02.c
@@ -1,20 +1,22 @@
-static void processEventDueNow(Time *time, ScheduledLightEvent *event)
+static void processEventsDueNow(Time *time, ScheduledLightEvent *event)
 {
     Day today = time->dayOfWeek;
     int minuteOfDay = time->minuteOfDay;
     if (event->id != UNUSED)
     {
         Day day = event->day;
+        /* if (isEventDueNow()) */
         if ((day == EVERYDAY) || (day == today) || (day == WEEKEND && (today == SATURDAY || today == SUNDAY)) ||
             (day == WEEKDAY && (today >= MONDAY && today <= FRIDAY)))
         {
-            /* it's the right day */
             if (minuteOfDay == event->minuteOfDay + event->randomMinutes)
             {
+                /* operateLight(); */
                 if (event->event == TURN_ON)
                     LightController_TurnOn(event->id);
                 else if (event->event == TURN_OFF)
                     LightController_TurnOff(event->id);
+                /* resetRandomize(); */
                 if (event->randomize == RANDOM_ON)
                     event->randomMinutes = RandomMinute_Get();
                 else
