7b99fc158647596683df69bd05cd7988261ea50c
Suho Kwon <suho.kwon@gmail.com>
2023-02-25 20:19:59 +0900

    Add g++ compiler standard options for Apple M1.

---
+++ Part06/SoundexCmake/build/CMakeFiles/soundextest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/soundextest.dir/SoundexTest.cpp.o"
+  "CMakeFiles/soundextest.dir/SoundexTest.cpp.o.d"
+  "CMakeFiles/soundextest.dir/main.cpp.o"
+  "CMakeFiles/soundextest.dir/main.cpp.o.d"
+  "soundextest"
+  "soundextest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/soundextest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
