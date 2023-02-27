928aa87448ec3396d1464585f932aa374026fde3
Suho Kwon <suho.kwon@gmail.com>
2023-02-27 10:18:42 +0900

    Add more examples.

---
+++ Part06/Soundex01/SoundexTest09.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include "gmock/gmock.h"
+using ::testing::Eq;
+
+class Soundex
+{
+public:
+    std::string encode(const std::string &word) const
+    {
+        return word;
+    }
+};
+
+TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
+{
+    Soundex soundex;
+    auto encoded = soundex.encode("A");
+    ASSERT_THAT(encoded, Eq("A"));
+}
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
+{
+    Soundex soundex;
+    auto encoded = soundex.encode("I");
+    ASSERT_THAT(encoded, Eq("I000"));
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ Part06/Soundex01/SoundexTest10.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include "gmock/gmock.h"
+using ::testing::Eq;
+
+class Soundex
+{
+public:
+    std::string encode(const std::string &word) const
+    {
+        return zeroPad(word);
+    }
+
+private:
+    std::string zeroPad(const std::string &word) const
+    {
+        return word + "000";
+    }
+};
+
+TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
+{
+    Soundex soundex;
+    auto encoded = soundex.encode("A");
+    ASSERT_THAT(encoded, Eq("A000"));
+}
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
+{
+    Soundex soundex;
+    auto encoded = soundex.encode("I");
+    ASSERT_THAT(encoded, Eq("I000"));
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
