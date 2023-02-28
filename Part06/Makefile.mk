08e1228998ff49c3590e822820a380f37821cf05
Suho Kwon <suho.kwon@gmail.com>
2023-02-28 16:11:49 +0900

    Add more examples.

--- Part06/Makefile.mk
+++ Part06/Makefile.mk
@@ -1,4 +1,5 @@
 CC= g++
+GOOGLETEST_OPTION= --gtest_catch_exceptions=0
 CFLAGS= -Wall --std=c++14
 CFLAGS_COV= -Wall -fprofile-arcs -ftest-coverage -g
 INCLUDES+= -I/usr/local/include/gtest \
@@ -9,7 +10,6 @@ LIBS= /usr/local/lib/libgmock.a \
 
 INCLUDE_FILES= $(INCLUDE_DIRS)/$(INCLUDE_FILE)
 INCLUDES= -I$(INCLUDE_DIRS)
-
 COVERAGE_INFO=testcoverage.info
 COVERAGE_OUTPUT_DIR=coverage
 .SILENT: $(TARGET) coverage clean cclean
@@ -18,13 +18,13 @@ $(TARGET): $(TARGET_SRC) $(SRCS) $(INCLUDE_FILES)
 	@echo "Building $(TARGET) with $(SRCS) $(INCLUDE_FILE)..."
 	@$(CC) -o $(TARGET) $(CFLAGS) $(SRCS) $(INCLUDES) $(LIBS)
 	@echo "Running $(TARGET)..."
-	@./$(TARGET)
+	@./$(TARGET) $(GOOGLETEST_OPTION)
 
 coverage: $(TARGET_SRC) $(SRCS) $(INCLUDE_FILES)
 	@echo "Building Test Coverage with $(SRCS) $(INCLUDE_FILE)..."
 	@$(CC) -o $(TARGET)_cov $(CFLAGS_COV) $(SRCS) $(INCLUDES) $(LIBS)
 	@echo "Running $(TARGET)..."
-	@-./$(TARGET)_cov
+	@-./$(TARGET)_cov $(GOOGLETEST_OPTION)
 	@gcov $(TARGET_SRC) 1>/dev/null
 	@lcov --rc lcov_branch_coverage=1 --capture --directory ./ --output-file $(COVERAGE_INFO) 1>/dev/null
 	@genhtml ${COVERAGE_INFO} --branch-coverage --output-directory ${COVERAGE_OUTPUT_DIR} 1>/dev/null
---
+++ Part06/Soundex03/Soundex14.cpp
@@ -0,0 +1,53 @@
+#include "Soundex06.h"
+
+static const size_t MaxCodeLength{4};
+
+string Soundex::encode(const string &word) const
+{
+    return zeroPad(head(word) + encodedDigits(tail(word)));
+}
+
+string Soundex::zeroPad(const string &word) const
+{
+    auto zerosNeeded = MaxCodeLength - word.length();
+    return word + string(zerosNeeded, '0');
+}
+
+string Soundex::head(const string &word) const
+{
+    return word.substr(0, 1);
+}
+
+string Soundex::tail(const string &word) const
+{
+    return word.substr(1);
+}
+
+/*
+string Soundex::encodedDigits(const string &word) const
+{
+    if (word.empty())
+        return "";
+    string encoding;
+    for (auto letter : word)
+        encoding += encodedDigit(letter);
+    return encoding;
+}
+*/
+
+string Soundex::encodedDigits(const string &word) const
+{
+    string encoding;
+    for (auto letter : word)
+        encoding += encodedDigit(letter);
+    return encoding;
+}
+
+string Soundex::encodedDigit(char letter) const
+{
+    const unordered_map<char, string> encodings{
+        {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"}, {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"}, {'d', "3"}, {'t', "3"}, {'l', "4"}, {'m', "5"}, {'n', "5"}, {'r', "6"}};
+
+    auto it = encodings.find(letter);
+    return it == encodings.end() ? "" : it->second;
+}
