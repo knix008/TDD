487160f2aeac84f09e51c5337b777823bc8f1a11
Suho Kwon <suho.kwon@gmail.com>
2023-03-03 14:19:30 +0900

    Add RetweetCollection example.

---
+++ Part07/RetweetCollection02/RetweetCollection.h
@@ -0,0 +1,13 @@
+#ifndef __RETWEETCOLLECTION_H__
+#define __RETWEETCOLLECTION_H__
+class RetweetCollection {
+public:
+   bool isEmpty() const {
+      return 0 == size();
+   }
+
+   unsigned int size() const {
+      return 0;
+   }
+};
+#endif
