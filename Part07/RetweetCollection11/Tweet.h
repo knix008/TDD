79fcc70db27a5c9c4672305dfa6387f9ef2f25a9
Suho Kwon <suho.kwon@gmail.com>
2023-03-04 21:06:49 +0900

    Add more examples.

---
+++ Part07/RetweetCollection11/Tweet.h
@@ -0,0 +1,33 @@
+#ifndef __TWEET_H__
+#define __TWEET_H__
+
+#include <string>
+
+class Tweet {
+public:
+   Tweet(const std::string& message="", const std::string& user="") 
+      : message_(message)
+      , user_(user) {
+   }
+
+   bool operator==(const Tweet& rhs) const {
+      return message_ == rhs.message_ &&
+         user_ == rhs.user_;
+   }
+
+   bool operator!=(const Tweet& rhs) const {
+      return !(*this == rhs);
+   }
+
+   bool operator<(const Tweet& rhs) const {
+      if (user_ == rhs.user_)
+         return message_ < rhs.message_;
+      return user_ < rhs.user_;
+   }
+
+private:
+   std::string message_;
+   std::string user_;
+};
+
+#endif
