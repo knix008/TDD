ff57a31c4d88b2be6c7d077ee409e723b098f67f
Suho Kwon <suho.kwon@gmail.com>
2023-03-04 21:22:46 +0900

    Add change in Makefile.

---
+++ Part07/RetweetCollection11/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/RetweetCollection.cpp.o"
+  "CMakeFiles/test.dir/RetweetCollectionTest.cpp.o"
+  "CMakeFiles/test.dir/Tweet.cpp.o"
+  "CMakeFiles/test.dir/TweetTest.cpp.o"
+  "CMakeFiles/test.dir/main.cpp.o"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
