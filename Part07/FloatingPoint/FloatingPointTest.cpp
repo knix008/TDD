7bccf5ae61bb040012ce9f1426beaec85c474920
Suho Kwon <suho.kwon@gmail.com>
2023-03-07 13:07:46 +0900

    Add floating point assertion example.

---
+++ Part07/FloatingPoint/FloatingPointTest.cpp
@@ -0,0 +1,16 @@
+#include "gmock/gmock.h"
+
+using namespace ::testing;
+using namespace std;
+
+class FloatingPoint: public Test {
+public:
+   double x{4.0};
+   double y{0.56};
+};
+
+TEST_F(FloatingPoint, IsSame) {
+   ASSERT_THAT(x + y, Eq(4.56));
+}
+
+
