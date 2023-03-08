c64d3c6465a365e19da211174a1a7236ae21fd29
Suho Kwon <suho.kwon@gmail.com>
2023-03-08 11:08:15 +0900

    Add GNU Compiler Collection example.(Not Working)

---
+++ Part07/GNUCompilerCollection11/Tweet.h
@@ -0,0 +1,52 @@
+#ifndef __TWEET_H__
+#define __TWEET_H__
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
+class InvalidUserException: public std::invalid_argument {
+public:
+   InvalidUserException(const std::string& user) 
+      : std::invalid_argument(user) {}
+};
+
+class Tweet {
+public:
+   static const std::string NULL_USER;
+   Tweet(const std::string& message="", 
+         const std::string& user=Tweet::NULL_USER) 
+      : message_(message)
+      , user_(user) {
+      if (!isValid(user_)) throw InvalidUserException(user_);
+   }
+
+   bool isValid(const std::string& user) const {
+      return '@' == user[0];
+   }
+
+   std::string user() const {
+      return user_;
+   }
+
+   bool operator==(const Tweet& rhs) const {
+      return message_ == rhs.message_ &&
+         user_ == rhs.user_;
+   }
+
+   bool operator!=(const Tweet& rhs) const {
+      return !(*this == rhs);
+   }
+
+   bool operator<(const Tweet& rhs) const {
+      if (user_ == rhs.user_)
+         return message_ < rhs.message_;
+      return user_ < rhs.user_;
+   }
+
+private:
+   std::string message_;
+   std::string user_;
+};
+
+#endif
