15820ec5bef61399db01f706f5fcb116bdafde8c
Suho Kwon <suho.kwon@gmail.com>
2023-03-09 11:16:35 +0900

    Add HTTP01 example.

--- Part08/HTTP01/Http.h
+++ Part08/HTTP01/Http.h
@@ -1,13 +1,5 @@
-/***
- * Excerpted from "Modern C++ Programming with Test-Driven Development",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/lotdd for more book information.
-***/
-#ifndef Http_h
-#define Http_h
+#ifndef __HTTP_H__
+#define __HTTP_H__
 
 #include <string>
 
