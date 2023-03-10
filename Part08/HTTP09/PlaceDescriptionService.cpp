f223dba2530dcc579ca79cea43a742e9a67f1420
Suho Kwon <suho.kwon@gmail.com>
2023-03-10 11:55:45 +0900

    Add refactored example.

--- Part08/HTTP09/PlaceDescriptionService.cpp
+++ Part08/HTTP09/PlaceDescriptionService.cpp
@@ -10,15 +10,16 @@ PlaceDescriptionService::PlaceDescriptionService(Http *http) : http_(http) {}
 
 string PlaceDescriptionService::summaryDescription(const string &latitude, const string &longitude) const
 {
-   string server{"http://open.mapquestapi.com/"};
-   string document{"nominatim/v1/reverse"};
-   string url = server + document + "?" +
-                keyValue("format", "json") + "&" +
-                keyValue("lat", latitude) + "&" +
-                keyValue("lon", longitude);
-
-   http_->get(url);
-   return "";
+    string server{"http://open.mapquestapi.com/"};
+    string document{"nominatim/v1/reverse"};
+    string url = server + document + "?" +
+                 keyValue("format", "json") + "&" +
+                 keyValue("lat", latitude) + "&" +
+                 keyValue("lon", longitude);
+    auto response = http_->get(url);
+    AddressExtractor extractor;
+    auto address = extractor.addressFrom(response);
+    return address.summaryDescription();
 }
 
 string PlaceDescriptionService::keyValue(const string &key, const string &value) const
