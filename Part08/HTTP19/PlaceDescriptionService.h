555a504ec5dab3480a3c03ebb977989348ef57cb
Suho Kwon <suho.kwon@gmail.com>
2023-03-13 10:25:03 +0900

    Add more examples.

---
+++ Part08/HTTP19/PlaceDescriptionService.h
@@ -0,0 +1,31 @@
+#ifndef __PLACEDESCRIPTIONSERVICE_H__
+#define __PLACEDESCRIPTIONSERVICE_H__
+
+#include <string>
+#include "Address.h"
+#include <curl/curl.h>
+#include <json/reader.h>
+#include <json/value.h>
+
+class PlaceDescriptionService
+{
+public:
+   PlaceDescriptionService();
+   ~PlaceDescriptionService()
+   {
+      curl_global_cleanup();
+   }
+   std::string summaryDescription(const std::string &latitude, const std::string &longitude) const;
+   static size_t writeCallback(const char *buf, size_t size, size_t nMemb, void *);
+
+private:
+   std::string createGetRequestUrl(const std::string &latitude, const std::string &longitude) const;
+   std::string summaryDescription(const Address &address) const;
+   std::string keyValue(const std::string &key, const std::string &value) const;
+   std::string getString(Json::Value &result, const std::string &name) const;
+
+   CURL *curl;
+   static std::string response_;
+};
+
+#endif
