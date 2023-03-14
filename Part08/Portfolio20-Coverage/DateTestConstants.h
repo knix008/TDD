42b1e1610c993bed66ace61bf3fc78ddc27268e1
Suho Kwon <suho.kwon@gmail.com>
2023-03-14 16:09:10 +0900

    Add test coverage example.

---
+++ Part08/Portfolio20-Coverage/DateTestConstants.h
@@ -0,0 +1,8 @@
+#ifndef __DATETESTCONSTANTS_H__
+#define __DATETESTCONSTANTS_H__
+
+#include "boost/date_time/gregorian/gregorian_types.hpp"
+
+static const boost::gregorian::date ArbitraryDate(2014, boost::gregorian::Sep, 5);
+
+#endif
