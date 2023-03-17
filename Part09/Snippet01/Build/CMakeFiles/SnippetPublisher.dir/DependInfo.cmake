64739eb16da17537a1b5408f01758dc901560d11
Suho Kwon <suho.kwon@gmail.com>
2023-03-17 16:12:43 +0900

    Add Snippet example.

---
+++ Part09/Snippet01/Build/CMakeFiles/SnippetPublisher.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shkwon/Projects/TDD/Part09/Snippet01/WavDescriptor.cpp" "/home/shkwon/Projects/TDD/Part09/Snippet01/Build/CMakeFiles/SnippetPublisher.dir/WavDescriptor.cpp.o"
+  "/home/shkwon/Projects/TDD/Part09/Snippet01/WavReader.cpp" "/home/shkwon/Projects/TDD/Part09/Snippet01/Build/CMakeFiles/SnippetPublisher.dir/WavReader.cpp.o"
+  "/home/shkwon/Projects/TDD/Part09/Snippet01/main.cpp" "/home/shkwon/Projects/TDD/Part09/Snippet01/Build/CMakeFiles/SnippetPublisher.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ATOMIC_NO_LIB"
+  "BOOST_FILESYSTEM_NO_LIB"
+  "BOOST_SYSTEM_NO_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/"
+  "/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
