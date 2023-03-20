7dda2f764caf45616a7c8340c0b5c62994ec3534
Suho Kwon <suho.kwon@gmail.com>
2023-03-20 13:52:13 +0900

    Add more test and classes.

---
+++ Part09/Snippet12/FileUtil.h
@@ -0,0 +1,25 @@
+#ifndef __FILEUTIL_H__
+#define __FILEUTIL_H__
+
+#include <ios>
+#include <istream>
+#include <fstream>
+#include <functional>
+
+class FileUtil {
+public:
+   std::streamsize size(const std::string& name) {
+      std::ifstream stream{name, std::ios::in | std::ios::binary};
+      stream.seekg(0, std::ios::end);
+      return stream.tellg();
+   }
+
+   std::streamsize execute(
+         const std::string& name, 
+         std::function<std::streamsize (std::istream&)> func) {
+      std::ifstream stream{name, std::ios::in | std::ios::binary};
+      return func(stream);
+   }
+};
+
+#endif
