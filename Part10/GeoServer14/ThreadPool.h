94cfac4406e3d903ccef42718fdc1c5c9348aaec
Suho Kwon <suho.kwon@gmail.com>
2023-03-22 19:22:48 +0900

    Add more examples.

---
+++ Part10/GeoServer14/ThreadPool.h
@@ -0,0 +1,77 @@
+#ifndef __THREADPOOL_H__
+#define __THREADPOOL_H__
+
+#include <string>
+#include <deque>
+#include <thread>
+#include <memory>
+#include <atomic>
+#include <mutex>
+#include <vector>
+
+#include "Work.h"
+
+class ThreadPool
+{
+public:
+   virtual ~ThreadPool()
+   {
+      stop();
+   }
+
+   void stop()
+   {
+      done_ = true;
+      for (auto &thread : threads_)
+         thread.join();
+   }
+
+   void start(unsigned int numberOfThreads = 1)
+   {
+      for (unsigned int i{0u}; i < numberOfThreads; i++)
+         threads_.push_back(std::thread(&ThreadPool::worker, this));
+   }
+
+   bool hasWork()
+   {
+      std::lock_guard<std::mutex> block(mutex_);
+      return !workQueue_.empty();
+   }
+
+   void add(Work work)
+   {
+      std::lock_guard<std::mutex> block(mutex_);
+      workQueue_.push_front(work);
+   }
+
+   Work pullWork()
+   {
+      std::lock_guard<std::mutex> block(mutex_);
+      if (workQueue_.empty())
+         return Work{};
+      auto work = workQueue_.back();
+      workQueue_.pop_back();
+      return work;
+   }
+
+private:
+   void worker()
+   {
+      while (!done_)
+      {
+         while (!done_ && !hasWork())
+            ;
+         if (done_)
+            break;
+         pullWork().execute();
+      }
+   }
+
+   std::atomic<bool> done_{false};
+   std::deque<Work> workQueue_;
+   std::shared_ptr<std::thread> workThread_;
+   std::mutex mutex_;
+   std::vector<std::thread> threads_;
+};
+
+#endif
