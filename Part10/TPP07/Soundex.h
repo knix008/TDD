cf8dd6ce59a74efe368d8c08d0176e4a4796aaef
Suho Kwon <suho.kwon@gmail.com>
2023-03-26 12:55:47 +0900

    Add more TPP examples.

---
+++ Part10/TPP07/Soundex.h
@@ -0,0 +1,47 @@
+#ifndef __SOUNDEX_H__
+#define __SOUNDEX_H__
+#include <string>
+
+class Soundex
+{
+public:
+   Soundex()
+   {
+      codes_['b'] = "1";
+      codes_['c'] = "2";
+   }
+
+   std::string encode(const std::string &word) const
+   {
+      std::string code("");
+      code += head(word) + encodeTail(word);
+      return zeroPad(code);
+   }
+
+   char head(const std::string &word) const
+   {
+      return word[0];
+   }
+
+   std::string encodeTail(const std::string &word) const
+   {
+      if (word[1] == 0)
+         return "";
+      return codes_[static_cast<size_t>(word[1])];
+   }
+
+   std::string zeroPad(const std::string &code) const
+   {
+      return code + (hasEncodedCharacters(code) ? "00" : "000");
+   }
+
+   bool hasEncodedCharacters(const std::string &code) const
+   {
+      return code[1] != 0;
+   }
+
+private:
+   std::string codes_[128];
+};
+
+#endif
