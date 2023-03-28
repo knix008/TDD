636360cfa08d0f91bb6b8fd1f58fe76aeb6f50e3
Suho Kwon <suho.kwon@gmail.com>
2023-03-28 14:47:51 +0900

    Add RomanConverter examples.

---
+++ Appendix/RomanConverter03/RomanConverterTest.cpp
@@ -0,0 +1,31 @@
+#include "gmock/gmock.h"
+
+using namespace ::testing;
+using namespace std;
+
+/*
+string convert(unsigned int arabic)
+{
+   return "I";
+}
+*/
+
+/*
+TEST(RomanConverter, CanConvertPositiveDigits)
+{
+   EXPECT_THAT(convert(1), Eq("I"));
+}
+*/
+
+string convert(unsigned int arabic)
+{
+   if (arabic == 2)
+      return "II";
+   return "I";
+}
+
+TEST(RomanConverter, CanConvertPositiveDigits)
+{
+   EXPECT_THAT(convert(1), Eq("I"));
+   EXPECT_THAT(convert(2), Eq("II"));
+}
